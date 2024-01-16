63a3bcda7e2b6f1e6702b07d303ee96120e8302d
gmind22 <gmind22@gmail.com>
2024-01-16 17:29:58 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PlayersList from "./PlayersList";
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center",}}>
+      <h1 style={{ color: "black", marginBottom: "2em", fontWeight: "bolder", marginTop: "30px"}}>Football Players App</h1>
+      <PlayersList />
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/players.js
@@ -0,0 +1,39 @@
+const players = [
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al-Nassr FC",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 36,
+      imageUrl: "https://m.media-amazon.com/images/I/51SXWL8Lb8L._AC_UY1100_.jpg",
+    },
+   
+    {
+        name: "Lionel Messi",
+        team: "Inter Miami",
+        nationality: "Argentina",
+        jerseyNumber: 10,
+        age: 36,
+        imageUrl: "https://www.si.com/.image/t_share/MTk5NDMyNTE5NzQzOTczMzI2/screen-shot-2023-07-17-at-52003-pm.png",
+      },
+
+      {
+        name: "Neymar",
+        team: "Al Hilal SFC",
+        nationality: "Portugal",
+        jerseyNumber: 10,
+        age: 31,
+        imageUrl: "https://m.media-amazon.com/images/I/41nGenPL-EL._AC_UY1100_.jpg",
+      },
+
+      {
+        name: "Robert Lewandowski",
+        team: "FC Barcelona",
+        nationality: "Poland",
+        jerseyNumber: 9,
+        age: 35,
+        imageUrl: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/b11dc20a-20c7-4193-bf57-6293fcd30ed8/robert-lewandowski-barcelona-2023-24-stadium-home-mens-dri-fit-soccer-jersey-Bvh6bg.png",
+      },
+  ];
+  
+  export default players;
